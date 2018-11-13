a1fdd06c063c629f33d42b10d993e4c7b1a60294
ALLINGTON <ea0911399@otc.edu>
2018-11-13 07:58:04 -0600

    made better

--- lotteryticket.py
+++ lotteryticket.py
@@ -7,7 +7,7 @@ import random
 def main():
     randomNumber()
     
-    createLotteryTicket()
+    createLotteryTicket(2)
 
 def randomNumber():
     digit1 = random.randint(0, 9)
@@ -17,10 +17,12 @@ def randomNumber():
     digit4 = random.randint(0, 9)
     digit5 = random.randint(0, 9)
     digit6 = random.randint(0, 9)
-    return randomNumber
+    digits = [digit1, digit2, digit3, digit4, digit5, digit6]
+    # print(f"{digits}")
+    createLotteryTicket(digits)
     
-def createLotteryTicket((randomNumber, digits):
-    ticket = [digits]
+def createLotteryTicket(ticketNumbers):
+    ticket = ticketNumbers
     # digit1 = random.randint(0, 9)
     # # print(digit1)
     # digit2 = random.randint(0, 9)
@@ -28,5 +30,5 @@ def createLotteryTicket((randomNumber, digits):
     # digit4 = random.randint(0, 9)
     # digit5 = random.randint(0, 9)
     # digit6 = random.randint(0, 9)
-    print(ticket)
+    print(f"Your lottery ticket number is {ticket}"")
 main()
